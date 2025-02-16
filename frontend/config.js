c8bef8a36f513616fe3b121d580c50d21b87768d
Sunny Yadav <67675772+SunnyYadav16@users.noreply.github.com>
2025-02-15 21:57:52 -0500

    Delete frontend directory

--- frontend/config.js
+++
@@ -1,7 +0,0 @@
-// config.js
-const config = {
-    apiEndpoint: 'http://localhost:8000/app/v1/youtube',
-};
-
-// Export the configuration for the current environment
-export const currentConfig = config
